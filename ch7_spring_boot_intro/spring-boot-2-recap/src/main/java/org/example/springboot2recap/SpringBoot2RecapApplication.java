15f2cc4b815df2e5ae891fe973ae19fc94602de6
Justin Wearing <justinwearing01@outlook.com>
2026-06-29 13:11:16 +0200

    Chapter 7: RECAP

---
+++ ch7_spring_boot_intro/spring-boot-2-recap/src/main/java/org/example/springboot2recap/SpringBoot2RecapApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot2recap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2RecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2RecapApplication.class, args);
+    }
+
+}
---
+++ ch7_spring_boot_intro/spring-boot-2-recap/src/test/java/org/example/springboot2recap/SpringBoot2RecapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot2recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2RecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
